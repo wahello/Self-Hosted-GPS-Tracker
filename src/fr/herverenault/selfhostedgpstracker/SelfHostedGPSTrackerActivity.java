fe4e20107c8008eb893a0c120b1f385002d9c5c5
Hervé Renault <hr@herverenault.fr>
2013-08-23 13:26:22 +0200

    Activity needs to listen to gps status, even if service is not running.

--- src/fr/herverenault/selfhostedgpstracker/SelfHostedGPSTrackerActivity.java
+++ src/fr/herverenault/selfhostedgpstracker/SelfHostedGPSTrackerActivity.java
@@ -5,18 +5,24 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class SelfHostedGPSTrackerActivity extends Activity {
+public class SelfHostedGPSTrackerActivity extends Activity implements LocationListener {
 
 	private final static String MY_TAG = "SelfHostedGPSTrackerActivity";
 	
+	private LocationManager locationManager;
+	
 	private TextView text_gps_status;
 	private TextView text_network_status;
+	
 	private BroadcastReceiver receiver = new BroadcastReceiver() {
 		@Override
 		public void onReceive(Context context, Intent intent) {
@@ -43,12 +49,20 @@ public class SelfHostedGPSTrackerActivity extends Activity {
 	public void onResume() {
 		super.onResume();
 		registerReceiver(receiver, new IntentFilter(SelfHostedGPSTrackerService.NOTIFICATION));
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 3, this); // TODO paramétrable !!!! (30 sec, 3 mètres)
+		if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+			onProviderEnabled(LocationManager.GPS_PROVIDER);
+		} else {
+			onProviderDisabled(LocationManager.GPS_PROVIDER);
+		}
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
 		unregisterReceiver(receiver);
+		locationManager.removeUpdates(this);
 	}
 
 	public void onToggleClicked(View view) {
@@ -70,4 +84,24 @@ public class SelfHostedGPSTrackerActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_self_hosted_gpstracker, menu);
 		return true;
 	} */
+	
+	/* -------------- GPS stuff -------------- */
+
+	@Override
+	public void onLocationChanged(Location location) {
+	}
+	
+	@Override
+	public void onProviderDisabled(String provider) {
+		text_gps_status.setText(getString(R.string.text_gps_status_disabled));
+	}
+	
+	@Override
+	public void onProviderEnabled(String provider) {
+		text_gps_status.setText(getString(R.string.text_gps_status_enabled));
+	}
+	
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+	}
 }
